2a3d8fe1ed7d3df4459522dd4b5cc0b120ebe39a
ray <brightmar@gmail.com>
2025-02-17 16:25:30 +0800

    add: version compare func

--- ADKit.swift
+++ ADKit.swift
@@ -8,6 +8,28 @@
 
 public class ADKit {
     
+    public static func compareVersion(_ version1: String, _ version2: String) -> Int {
+        let v1 = version1.split(separator: ".").map { Int($0) ?? 0 }
+        let v2 = version2.split(separator: ".").map { Int($0) ?? 0 }
+        
+        // 获取最长的长度
+        let maxLength = max(v1.count, v2.count)
+        
+        // 比较每个版本号部分
+        for i in 0..<maxLength {
+            let num1 = i < v1.count ? v1[i] : 0
+            let num2 = i < v2.count ? v2[i] : 0
+            
+            if num1 > num2 {
+                return 1
+            } else if num1 < num2 {
+                return -1
+            }
+        }
+        
+        return 0
+    }
+    
     public static var version: String {
         guard let dictionary = Bundle.init(for: ADKit.self).infoDictionary else {
             return "unknown"
