4dcfa11af30214f01b9427f637ad2edf226a8019
ray <brightmar@gmail.com>
2026-01-13 09:56:20 +0800

    fix: waitOnce

--- Classes/Extensions/Publisher+WaitOnce.swift
+++ Classes/Extensions/Publisher+WaitOnce.swift
@@ -32,11 +32,11 @@ extension Publisher where Failure == Never {
     public func waitOnce<T: Scheduler>(until: @escaping (Output) -> Bool = {_ in true},
                                        scheduler: T = DispatchQueue.main,
                                        options: T.SchedulerOptions? = nil,
-                                       timeoutSeconds: TimeInterval,
+                                       timeout: T.SchedulerTimeType.Stride,
                                        result: @escaping (Result<Output, TimeoutError>) -> Void) -> AnyCancellable {
         setFailureType(to: TimeoutError.self).first(where: {
             until($0)
-        }).timeout(.seconds(timeoutSeconds), scheduler: scheduler, options: options)
+        }).timeout(timeout, scheduler: scheduler, options: options)
             .receive(on: scheduler)
             .sink { res in
             switch res {
@@ -53,10 +53,10 @@ extension Publisher where Failure == Never {
     public func waitOnce<T: Scheduler>(until: @escaping (Output) -> Bool = {_ in true},
                                        scheduler: T = DispatchQueue.main,
                                        options: T.SchedulerOptions? = nil,
-                                       timeoutSeconds: TimeInterval,
+                                       timeout: T.SchedulerTimeType.Stride,
                                        setCancelation with: inout AnyCancellable?) async -> Result<Output, TimeoutError> {
         await withCheckedContinuation { continuation in
-            with = waitOnce(until: until, scheduler: scheduler, options: options, timeoutSeconds: timeoutSeconds) { res in
+            with = waitOnce(until: until, scheduler: scheduler, options: options, timeout: timeout) { res in
                 continuation.resume(returning: res)
             }
         }
@@ -65,11 +65,11 @@ extension Publisher where Failure == Never {
     public func waitOnce<T: Scheduler>(until: @escaping (Output) -> Bool = {_ in true},
                                        scheduler: T = DispatchQueue.main,
                                        options: T.SchedulerOptions? = nil,
-                                       timeoutSeconds: TimeInterval,
+                                       timeout: T.SchedulerTimeType.Stride,
                                        setCancelation to: Associatable,
                                        cancelationOptions: (key: String, doNotStoreIfHasSameKey: Bool)? = nil) async -> Result<Output, TimeoutError> {
         await withCheckedContinuation { continuation in
-            waitOnce(until: until, scheduler: scheduler, options: options, timeoutSeconds: timeoutSeconds) { res in
+            waitOnce(until: until, scheduler: scheduler, options: options, timeout: timeout) { res in
                 continuation.resume(returning: res)
             }.ry.store(to: to, with: cancelationOptions?.key, doNotStoreIfHasSameKey: cancelationOptions?.doNotStoreIfHasSameKey ?? false)
         }
@@ -78,28 +78,48 @@ extension Publisher where Failure == Never {
 
 extension Publisher where Output: Equatable, Failure == Never {
     
-    public func waitOnce<T: Scheduler>(_ output: Output,
+    public func waitOnce<T: Scheduler>(for output: Output,
                                        scheduler: T = DispatchQueue.main,
                                        options: T.SchedulerOptions? = nil,
-                                       timeoutSeconds: TimeInterval,
+                                       timeout: T.SchedulerTimeType.Stride,
                                        result: @escaping (Result<Output, TimeoutError>) -> Void) -> AnyCancellable {
-        waitOnce(until: { $0 == output }, scheduler: scheduler, options: options, timeoutSeconds: timeoutSeconds, result: result)
+        waitOnce(until: { $0 == output }, scheduler: scheduler, options: options, timeout: timeout, result: result)
     }
     
-    public func waitOnce<T: Scheduler>(_ output: Output,
+    public func waitOnce<T: Scheduler>(for output: Output,
                                        scheduler: T = DispatchQueue.main,
                                        options: T.SchedulerOptions? = nil,
-                                       timeoutSeconds: TimeInterval,
+                                       timeout: T.SchedulerTimeType.Stride,
                                        setCancelation with: inout AnyCancellable?) async -> Result<Output, TimeoutError> {
-        await waitOnce(until: { $0 == output }, scheduler: scheduler, options: options, timeoutSeconds: timeoutSeconds, setCancelation: &with)
+        await waitOnce(until: { $0 == output }, scheduler: scheduler, options: options, timeout: timeout, setCancelation: &with)
     }
     
-    public func waitOnce<T: Scheduler>(_ output: Output,
+    public func waitOnce<T: Scheduler>(for output: Output,
                                        scheduler: T = DispatchQueue.main,
                                        options: T.SchedulerOptions? = nil,
-                                       timeoutSeconds: TimeInterval,
+                                       timeout: T.SchedulerTimeType.Stride,
                                        setCancelation to: Associatable,
                                        cancelationOptions: (key: String, doNotStoreIfHasSameKey: Bool)? = nil) async -> Result<Output, TimeoutError> {
-        await waitOnce(until: { $0 == output }, scheduler: scheduler, options: options, timeoutSeconds: timeoutSeconds, setCancelation: to, cancelationOptions: cancelationOptions)
+        await waitOnce(until: { $0 == output }, scheduler: scheduler, options: options, timeout: timeout, setCancelation: to, cancelationOptions: cancelationOptions)
+    }
+}
+
+extension DispatchTimeInterval {
+    
+    public var stride: DispatchQueue.SchedulerTimeType.Stride {
+        switch self {
+        case .seconds(let int):
+            .seconds(int)
+        case .milliseconds(let int):
+            .milliseconds(int)
+        case .microseconds(let int):
+            .microseconds(int)
+        case .nanoseconds(let int):
+            .nanoseconds(int)
+        case .never:
+            .seconds(.infinity)
+        @unknown default:
+            .zero
+        }
     }
 }
--- Classes/Lock/LockWrapper.swift
+++ Classes/Lock/LockWrapper.swift
@@ -8,36 +8,83 @@
 import Foundation
 import System
 
-@propertyWrapper
-public class LockWrapper<T>: @unchecked Sendable {
+public protocol AnyMutex {
+    mutating func install()
+    mutating func lock()
+    mutating func unlock()
+    mutating func dispose()
+}
+
+extension AnyMutex {
+    public func install() {}
+    public func dispose() {}
+}
+
+extension NSLock: AnyMutex {}
+
+extension os_unfair_lock: AnyMutex {
+    public mutating func lock() { os_unfair_lock_lock(&self) }
+    public mutating func unlock() { os_unfair_lock_unlock(&self) }
+}
+extension pthread_mutex_t: AnyMutex {
+    public mutating func install() { pthread_mutex_init(&self, nil) }
+    public mutating func lock() { pthread_mutex_lock(&self) }
+    public mutating func unlock() { pthread_mutex_unlock(&self) }
+    public mutating func dispose() { pthread_mutex_destroy(&self) }
+}
+
+public class LockReferWrapper<T, L: AnyMutex>: @unchecked Sendable {
     
-    private var mutex = os_unfair_lock()
+    private var mutex: L
+    private var _wrappedValue: T
     
-    public var _wrappedValue: T
     public var wrappedValue: T {
         set {
             defer {
-                os_unfair_lock_unlock(&mutex)
+                mutex.lock()
             }
-            os_unfair_lock_lock(&mutex)
+            mutex.unlock()
             _wrappedValue = newValue
         }
         get {
             defer {
-                os_unfair_lock_unlock(&mutex)
+                mutex.lock()
             }
-            os_unfair_lock_lock(&mutex)
+            mutex.unlock()
             return _wrappedValue
         }
     }
 
-    public init(wrappedValue: T) {
+    public init(wrappedValue: T, mutex: L) {
         _wrappedValue = wrappedValue
+        self.mutex = mutex
+        self.mutex.install()
+    }
+    
+    deinit {
+        mutex.dispose()
     }
 }
 
 @propertyWrapper
-public class RWLockWrapper<T>: @unchecked Sendable {
+public class LockWrapper<T>: LockReferWrapper<T, os_unfair_lock>, @unchecked Sendable {
+    
+    public override var wrappedValue: T {
+        set {
+            super.wrappedValue = newValue
+        }
+        get {
+            super.wrappedValue
+        }
+    }
+    
+    public init(wrappedValue: T) {
+        super.init(wrappedValue: wrappedValue, mutex: os_unfair_lock_s())
+    }
+}
+
+@propertyWrapper
+public class RWLockWrapper<T> {
     
     private var mutex = pthread_rwlock_t()
     
