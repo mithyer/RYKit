964276ba0b825908fcad57d2030baa52b50f8fb2
ray <brightmar@gmail.com>
2026-01-20 11:27:28 +0800

    feat: String subscript int range

---
+++ Classes/Extensions/String+Subscript.swift
@@ -0,0 +1,43 @@
+//
+//  String+IntSub.swift
+//  RYKit
+//
+//  Created by mao rui on 2026/1/20.
+//
+
+extension String {
+
+    subscript(_ range: Range<Int>) -> Substring {
+        let lower = max(0, range.lowerBound)
+        let upper = min(self.count, range.upperBound)
+
+        guard lower < upper else { return "" }
+
+        let startIndex = self.index(self.startIndex, offsetBy: lower)
+        let endIndex = self.index(self.startIndex, offsetBy: upper)
+        
+        return self[startIndex..<endIndex]
+    }
+
+    subscript(_ range: ClosedRange<Int>) -> Substring {
+        let lower = max(0, range.lowerBound)
+        let upper = min(self.count - 1, range.upperBound)
+
+        guard lower <= upper else { return "" }
+
+        let startIndex = self.index(self.startIndex, offsetBy: lower)
+        let endIndex = self.index(self.startIndex, offsetBy: upper)
+
+        return self[startIndex...endIndex]
+    }
+    
+    subscript(_ range: Range<Int>) -> String {
+        let sub: Substring = self[range]
+        return String(sub)
+    }
+
+    subscript(_ range: ClosedRange<Int>) -> String {
+        let sub: Substring = self[range]
+        return String(sub)
+    }
+}
