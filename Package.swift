c2fe4de5920dcf1c35465c20d33251b08660f02d
ray <brightmar@gmail.com>
2026-03-30 09:21:40 +0800

    build: add base Package.swift manifest

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "RYKit",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13)
+    ],
+    products: [],
+    targets: []
+)
